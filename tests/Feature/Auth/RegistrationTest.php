9d471c9e8dd77f43385e9a61ccc6f6e7aaa4300d
teoman <mrfoundman998@gmail.com>
2026-06-01 09:51:55 +0200

    fix: Update redirect route for new user registration to point to home

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -30,7 +30,7 @@ class RegistrationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
+        $response->assertRedirect(route('home', absolute: false));
 
         Mail::assertSentCount(1);
 
